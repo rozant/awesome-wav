5f92d10fa3976b431e5184a40c1e809ba5f95ec9
mcmult <mcmult@e155a4ec-1519-11df-b09a-cf1a22568e95>
2010-03-16 21:42:18 +0000

    added test suite location and bash script.  script to be finished still.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 #ifdef _DEBUG
 char DEBUG_WAV[] = "./The Blood Of Cu Chulainn-16_Bit_PCM.wav";
-char DEBUG_DATA[] = "./vlc.exe";
+char DEBUG_DATA[] = "./test_input-1b-16kbps-43582508.txt";
 char DEBUG_ENCODED_WAV[] = "E_song.wav";
 char DEBUG_DECODED_DATA[] = "D_data.txt";
 #endif
