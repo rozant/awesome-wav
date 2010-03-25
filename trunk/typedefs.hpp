78f8a825e7ffbf648076bc8fc699c548c9a15d15
rozant@rpi.edu <rozant@rpi.edu@e155a4ec-1519-11df-b09a-cf1a22568e95>
2010-03-25 20:33:19 +0000

    Forgot to add our new typedefs.hpp to the svn

---
+++ trunk/typedefs.hpp
@@ -0,0 +1,15 @@
+#ifndef __typedefs_hpp__
+#define __typedefs_hpp__
+
+/* typedefs to make things look nice */
+#ifdef _WIN32
+	typedef unsigned __int32 DWORD; 
+	typedef unsigned __int16 SHORT;
+	typedef unsigned __int8 BYTE;
+#else
+	typedef uint32_t DWORD; 
+	typedef uint16_t SHORT;
+	typedef uint8_t BYTE;
+#endif
+
+#endif
