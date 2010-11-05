318e8f48bd846a4cfdfc05f7d2652350df1f468d
rozant@rpi.edu <rozant@rpi.edu@e155a4ec-1519-11df-b09a-cf1a22568e95>
2010-11-05 03:45:37 +0000

    Initial FLAC files.

---
+++ src/flac_struct.hpp
@@ -0,0 +1,161 @@
+/*****************************************************************
+* This program is free software; you can redistribute it and/or	 *
+* modify it under the terms of the GNU General Public License	 *
+* version 2 as published by the Free Software Foundation.		 *
+*																 *
+* This program is distributed in the hope that it will be		 *
+* useful, but WITHOUT ANY WARRANTY; without even the implied	 *
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR		 *
+* PURPOSE.  See the GNU General Public License for more details. *
+*																 *
+* You should have received a copy of the GNU General Public 	 *
+* License along with this program; if not, write to the Free 	 *
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,	 *
+* USA.															 *
+*****************************************************************/
+#ifndef __flac_struct_hpp__
+#define __flac_struct_hpp__
+#include "global.hpp"
+#include <stdlib.h>
+
+struct _METADATA_BLOCK_STREAMINFO {
+	_METADATA_BLOCK_STREAMINFO(void) { return; }
+	~_METADATA_BLOCK_STREAMINFO(void) { return; }
+};
+
+struct _METADATA_BLOCK_PADDING {
+	_METADATA_BLOCK_PADDING(void) { return; }
+	~_METADATA_BLOCK_PADDING(void) { return; }
+};
+
+struct _METADATA_BLOCK_APPLICATION {
+	_METADATA_BLOCK_APPLICATION(void) { return; }
+	~_METADATA_BLOCK_APPLICATION(void) { return; }
+};
+
+struct _METADATA_BLOCK_SEEKTABLE {
+	_METADATA_BLOCK_SEEKTABLE(void) { return; }
+	~_METADATA_BLOCK_SEEKTABLE(void) { return; }
+};
+
+struct _SEEKPOINT {
+	_SEEKPOINT(void) { return; }
+	~_SEEKPOINT(void) { return; }
+};
+
+struct _METADATA_BLOCK_VORBIS_COMMENT {
+	_METADATA_BLOCK_VORBIS_COMMENT(void) { return; }
+	~_METADATA_BLOCK_VORBIS_COMMENT(void) { return; }
+};
+
+struct CUESHEET_INDEX {
+	CUESHEET_INDEX(void) { return; }
+	~CUESHEET_INDEX(void) { return; }
+};
+
+struct CUESHEET_TRACK {
+	CUESHEET_TRACK(void) { return; }
+	~CUESHEET_TRACK(void) { return; }
+};
+
+struct _METADATA_BLOCK_CUESHEET {
+	_METADATA_BLOCK_CUESHEET(void) { return; }
+	~_METADATA_BLOCK_CUESHEET(void) { return; }
+};
+
+struct _METADATA_BLOCK_PICTURE {
+	_METADATA_BLOCK_PICTURE(void) { return; }
+	~_METADATA_BLOCK_PICTURE(void) { return; }
+};
+
+struct _FRAME_HEADER {
+	_FRAME_HEADER(void) { return; }
+	~_FRAME_HEADER(void) { return; }
+};
+
+struct _RICE_PARTITION {
+	_RICE_PARTITION(void) { return; }
+	~_RICE_PARTITION(void) { return; }
+};
+
+struct _RESIDUAL_CODING_METHOD_PARTITIONED_RICE {
+	_RESIDUAL_CODING_METHOD_PARTITIONED_RICE(void) { return; }
+	~_RESIDUAL_CODING_METHOD_PARTITIONED_RICE(void) { return; }
+};
+
+struct RICE2_PARTITION {
+	RICE2_PARTITION(void) { return; }
+	~RICE2_PARTITION(void) { return; }
+};
+
+struct RESIDUAL_CODING_METHOD_PARTITIONED_RICE2 {
+	RESIDUAL_CODING_METHOD_PARTITIONED_RICE2(void) { return; }
+	~RESIDUAL_CODING_METHOD_PARTITIONED_RICE2(void) { return; }
+};
+
+struct _RESIDUAL {
+	_RESIDUAL(void) { return; }
+	~_RESIDUAL(void) { return; }
+};
+
+struct _SUBFRAME_HEADER {
+	_SUBFRAME_HEADER(void) { return; }
+	~_SUBFRAME_HEADER(void) { return; }
+};
+
+struct _SUBFRAME_CONSTANT {
+	_SUBFRAME_CONSTANT(void) { return; }
+	~_SUBFRAME_CONSTANT(void) { return; }
+};
+
+struct _SUBFRAME_FIXED {
+	_SUBFRAME_FIXED(void) { return; }
+	~_SUBFRAME_FIXED(void) { return; }
+};
+
+struct _SUBFRAME_LPC {
+	_SUBFRAME_LPC(void) { return; }
+	~_SUBFRAME_LPC(void) { return; }
+};
+
+struct _SUBFRAME_VERBATIM {
+	_SUBFRAME_VERBATIM(void) { return; }
+	~_SUBFRAME_VERBATIM(void) { return; }
+};
+
+struct _FRAME_FOOTER {
+	_FRAME_FOOTER(void) { return; }
+	~_FRAME_FOOTER(void) { return; }
+};
+
+struct _FRAME {
+	_FRAME(void) { return; }
+	~_FRAME(void) { return; }
+};
+
+struct _METADATA_BLOCK_HEADER {
+	_METADATA_BLOCK_HEADER(void) { return; }
+	~_METADATA_BLOCK_HEADER(void) { return; }
+};
+
+struct _METADATA_BLOCK_DATA {
+	_METADATA_BLOCK_DATA(void) { return; }
+	~_METADATA_BLOCK_DATA(void) { return; }
+};
+
+struct _METADATA_BLOCK {
+	_METADATA_BLOCK(void) { return; }
+	~_METADATA_BLOCK(void) { return; }
+};
+
+struct _STREAM {
+	_STREAM(void) { return; }
+	~_STREAM(void) { return; }
+};
+
+#endif
+
+/****************************************************************/
+/****************************************************************/
+/****************************************************************/
+
