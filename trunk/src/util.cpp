85c3858bfde0120d7592231158bdfcf05d16a312
mcmult <mcmult@e155a4ec-1519-11df-b09a-cf1a22568e95>
2013-02-12 20:25:48 +0000

    dont overload the c close function

--- trunk/src/util.cpp
+++ trunk/src/util.cpp
@@ -1,113 +1,113 @@
 /*****************************************************************
-* This program is free software; you can redistribute it and/or	 *
-* modify it under the terms of the GNU General Public License	 *
-* version 2 as published by the Free Software Foundation.		 *
-*																 *
-* This program is distributed in the hope that it will be		 *
-* useful, but WITHOUT ANY WARRANTY; without even the implied	 *
-* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR		 *
+* This program is free software; you can redistribute it and/or  *
+* modify it under the terms of the GNU General Public License    *
+* version 2 as published by the Free Software Foundation.        *
+*                                                                *
+* This program is distributed in the hope that it will be        *
+* useful, but WITHOUT ANY WARRANTY; without even the implied     *
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR        *
 * PURPOSE.  See the GNU General Public License for more details. *
-*																 *
-* You should have received a copy of the GNU General Public 	 *
-* License along with this program; if not, write to the Free 	 *
-* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,	 *
-* USA.															 *
+*                                                                *
+* You should have received a copy of the GNU General Public      *
+* License along with this program; if not, write to the Free     *
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,  *
+* USA.                                                           *
 *****************************************************************/
 #include "util.hpp"
 #include "logger.hpp"
 
 /****************************************************************/
-/* function: open_file											*/
-/* purpose: open a file.										*/
-/* args: const char *, const char * 							*/
-/* returns: FILE *												*/
-/*		*	 = opened correctly									*/
-/*		NULL = opened incorrectly								*/
+/* function: open_file                                          */
+/* purpose: open a file.                                        */
+/* args: const char *, const char *                             */
+/* returns: FILE *                                              */
+/*        *     = opened correctly                              */
+/*        NULL = opened incorrectly                             */
 /****************************************************************/
 FILE* open_file(const char *filename, const char *mode) {
-	FILE* aFile = fopen(filename, mode);
+    FILE* aFile = fopen(filename, mode);
 
-	if (aFile == NULL) {
-		#ifdef _DEBUGOUTPUT
-		LOG_DEBUG("E: Failed to open %s with mode %s\n", filename, mode);
-		#else
-		LOG("Failed to open %s with mode %s\n", filename, mode);
-		#endif
-	} else {
-		LOG_DEBUG("S: Opened %s with mode %s\n", filename, mode);
-	}
+    if (aFile == NULL) {
+        #ifdef _DEBUGOUTPUT
+        LOG_DEBUG("E: Failed to open %s with mode %s\n", filename, mode);
+        #else
+        LOG("Failed to open %s with mode %s\n", filename, mode);
+        #endif
+    } else {
+        LOG_DEBUG("S: Opened %s with mode %s\n", filename, mode);
+    }
 
-	return aFile;
+    return aFile;
 }
 
 /****************************************************************/
-/* function: close												*/
-/* purpose: close an open file.									*/
-/* args: FILE *													*/
-/* returns: bool												*/
-/*		1 = closed correctly									*/
-/*		0 = closed incorrectly, or already closed				*/
+/* function: close_file                                         */
+/* purpose: close an open file.                                 */
+/* args: FILE *                                                 */
+/* returns: bool                                                */
+/*        1 = closed correctly                                  */
+/*        0 = closed incorrectly, or already closed             */
 /****************************************************************/
-bool close(FILE *aFile) {
-	if (aFile) {
-		if (fclose(aFile)) {
-			LOG_DEBUG("E: Failed to close file\n");
-			return false;
-		} else {
-			aFile = NULL;
-			LOG_DEBUG("S: Closed file\n");
-			return true;
-		}
-	}
-	LOG_DEBUG("E: File already closed\n");
-	return false;
+bool close_file(FILE *aFile) {
+    if (aFile) {
+        if (fclose(aFile)) {
+            LOG_DEBUG("E: Failed to close file\n");
+            return false;
+        } else {
+            aFile = NULL;
+            LOG_DEBUG("S: Closed file\n");
+            return true;
+        }
+    }
+    LOG_DEBUG("E: File already closed\n");
+    return false;
 }
 
 /****************************************************************/
-/* function: safeRemove											*/
-/* purpose: overwrites a file with 0s before removing it		*/
-/* args: const char *											*/
-/* returns: int													*/
-/*		0 = overwrote and removed file successfully				*/
+/* function: safeRemove                                         */
+/* purpose: overwrites a file with 0s before removing it        */
+/* args: const char *                                           */
+/* returns: int                                                 */
+/*        0 = overwrote and removed file successfully           */
 /****************************************************************/
 int safeRemove(const char *filename) {
-	long int fileSize = 0, bufferSize = 128 * BUFFER_MULT;
-	int8 buffer[128 * BUFFER_MULT];
-	int result = 1;
-	FILE *aFile;
+    long int fileSize = 0, bufferSize = 128 * BUFFER_MULT;
+    int8 buffer[128 * BUFFER_MULT];
+    int result = 1;
+    FILE *aFile;
 
-	aFile = fopen(filename, "rb+");
-	
-	if (aFile) {
-		fseek(aFile, 0, SEEK_END);
-		fileSize = ftell(aFile);
-		fseek(aFile, 0, SEEK_SET);
+    aFile = fopen(filename, "rb+");
+    
+    if (aFile) {
+        fseek(aFile, 0, SEEK_END);
+        fileSize = ftell(aFile);
+        fseek(aFile, 0, SEEK_SET);
 
-		memset(buffer, 0, bufferSize);
+        memset(buffer, 0, bufferSize);
 
-		while (fileSize > 0) {
-			if (fileSize - bufferSize > 0) {
-				fileSize -= bufferSize;
-			} else {
-				bufferSize = fileSize;
-				fileSize = 0;
-			}
+        while (fileSize > 0) {
+            if (fileSize - bufferSize > 0) {
+                fileSize -= bufferSize;
+            } else {
+                bufferSize = fileSize;
+                fileSize = 0;
+            }
 
-			fwrite(buffer, sizeof(int8), bufferSize, aFile);
-		}
+            fwrite(buffer, sizeof(int8), bufferSize, aFile);
+        }
 
-		fclose(aFile);
+        fclose(aFile);
 
-		result = remove(filename);
-	}
+        result = remove(filename);
+    }
 
-	if (result == 0) {
-		LOG_DEBUG("S: Successfully overwrote and removed %s\n", filename);
-	} else {
-		LOG_DEBUG("E: Failed to overwrite and remove %s\n", filename);
-	}
+    if (result == 0) {
+        LOG_DEBUG("S: Successfully overwrote and removed %s\n", filename);
+    } else {
+        LOG_DEBUG("E: Failed to overwrite and remove %s\n", filename);
+    }
 
-	return result;
+    return result;
 }
 
 /****************************************************************/
--- trunk/src/util.hpp
+++ trunk/src/util.hpp
@@ -1,17 +1,17 @@
 /*****************************************************************
-* This program is free software; you can redistribute it and/or	 *
-* modify it under the terms of the GNU General Public License	 *
-* version 2 as published by the Free Software Foundation.		 *
-*																 *
-* This program is distributed in the hope that it will be		 *
-* useful, but WITHOUT ANY WARRANTY; without even the implied	 *
-* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR		 *
+* This program is free software; you can redistribute it and/or  *
+* modify it under the terms of the GNU General Public License    *
+* version 2 as published by the Free Software Foundation.        *
+*                                                                *
+* This program is distributed in the hope that it will be        *
+* useful, but WITHOUT ANY WARRANTY; without even the implied     *
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR        *
 * PURPOSE.  See the GNU General Public License for more details. *
-*																 *
-* You should have received a copy of the GNU General Public 	 *
-* License along with this program; if not, write to the Free 	 *
-* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,	 *
-* USA.															 *
+*                                                                *
+* You should have received a copy of the GNU General Public      *
+* License along with this program; if not, write to the Free     *
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,  *
+* USA.                                                           *
 *****************************************************************/
 #ifndef __util_hpp__
 #define __util_hpp__
@@ -21,105 +21,105 @@
 
 // non inline functions
 FILE* open_file(const char *filename, const char *mode);
-bool close(FILE *aFile);
+bool close_file(FILE *aFile);
 int safeRemove(const char *filename);
 
 /****************************************************************/
-/* function: setBit												*/
-/* purpose: sets the bit at a specific position					*/
-/* args: int8&, const int8, const bool							*/
+/* function: setBit                                             */
+/* purpose: sets the bit at a specific position                 */
+/* args: int8&, const int8, const bool                          */
 /****************************************************************/
 inline void setBit(int8 &b, const int8 index, const bool torf) {
-	if (torf) // Set bit to 1
-		b |= (1 << index); 
-	else // Set bit to 0
-		b &= ~(1 << index);
+    if (torf) // Set bit to 1
+        b |= (1 << index); 
+    else // Set bit to 0
+        b &= ~(1 << index);
 }
 
 /****************************************************************/
-/* function: getBit												*/
-/* purpose: gets the bit at a specific position					*/
-/* args: const int8, const int8									*/
-/* returns: bool												*/
-/*		1 = bit is a 1											*/
-/*		0 = bit is a 0											*/
+/* function: getBit                                             */
+/* purpose: gets the bit at a specific position                 */
+/* args: const int8, const int8                                 */
+/* returns: bool                                                */
+/*        1 = bit is a 1                                        */
+/*        0 = bit is a 0                                        */
 /****************************************************************/
 inline bool getBit(const int8 b, const int8 index) {
-	return (bool)((1 << index) & b);
+    return (bool)((1 << index) & b);
 }
 
 /****************************************************************/
-/* function: clearLower2Bits									*/
-/* purpose: clears the 2 lower bits in a int8					*/
-/* args: int8&													*/
+/* function: clearLower2Bits                                    */
+/* purpose: clears the 2 lower bits in a int8                   */
+/* args: int8&                                                  */
 /****************************************************************/
 inline void clearLower2Bits(int8 &b) {
-	b &= 0xFC; // 252
+    b &= 0xFC; // 252
 }
 
 /****************************************************************/
-/* function: clearLower2Bits_off								*/
-/* purpose: clears the 2 lower bits in a int8, with offset		*/
-/* args: int8&, const char										*/
-/* returns: int													*/
+/* function: clearLower2Bits_off                                */
+/* purpose: clears the 2 lower bits in a int8, with offset      */
+/* args: int8&, const char                                      */
+/* returns: int                                                 */
 /****************************************************************/
 inline int clearLower2Bits_off(int8 &b, const char off) {
-	switch (off) {
-		case 0:
-			b &= 0xFC; // 252
-			break;
-		case 1:
-			b &= 0xF9; // 249
-			break;
-		case 2:
-			b &= 0xF3; // 243
-			break;
-		case 3:
-			b &= 0xE7; // 231
-			break;
-		case 4:
-			b &= 0xCF; // 207
-			break;
-		case 5:
-			b &= 0x9F; // 159
-			break;
-		case 6:
-			b &= 0x3F; // 63
-			break;
-		default:
-			return -1;
-			break;
-	}
-	return 0;
+    switch (off) {
+        case 0:
+            b &= 0xFC; // 252
+            break;
+        case 1:
+            b &= 0xF9; // 249
+            break;
+        case 2:
+            b &= 0xF3; // 243
+            break;
+        case 3:
+            b &= 0xE7; // 231
+            break;
+        case 4:
+            b &= 0xCF; // 207
+            break;
+        case 5:
+            b &= 0x9F; // 159
+            break;
+        case 6:
+            b &= 0x3F; // 63
+            break;
+        default:
+            return -1;
+            break;
+    }
+    return 0;
 }
 
 /****************************************************************/
-/* function: clearLower4Bits									*/
-/* purpose: clears the 4 lower bits in a int8					*/
-/* args: int8&													*/
+/* function: clearLower4Bits                                    */
+/* purpose: clears the 4 lower bits in a int8                   */
+/* args: int8&                                                  */
 /****************************************************************/
 inline void clearLower4Bits(int8 &b) {
-	b &= 0xF0; // 240
+    b &= 0xF0; // 240
 }
 
 /****************************************************************/
-/* function: clearUpper4Bits									*/
-/* purpose: clears the 4 upper bits in a int8					*/
-/* args: int8&													*/
+/* function: clearUpper4Bits                                    */
+/* purpose: clears the 4 upper bits in a int8                   */
+/* args: int8&                                                  */
 /****************************************************************/
 inline void clearUpper4Bits(int8 &b) {
-	b &= 0x0F; // 15
+    b &= 0x0F; // 15
 }
 
 /****************************************************************/
-/* function: byteToMB											*/
-/* purpose: convers bytes to megabytes							*/
-/* args: const int32											*/
-/* returns: double												*/
+/* function: byteToMB                                           */
+/* purpose: convers bytes to megabytes                          */
+/* args: const int32                                            */
+/* returns: double                                              */
 /****************************************************************/
 #ifdef _DEBUGOUTPUT
 inline double byteToMB(const int32 bytes) {
-	return bytes / 1048576.0;
+    return bytes / 1048576.0;
 }
 #endif
 
